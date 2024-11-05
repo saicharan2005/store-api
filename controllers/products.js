eab6a0319f26f0a413ae62b76588007b3926b8ab
saicharan2005 <kundurusaicharan@gmail.com>
2024-11-05 12:39:17 +0530

    added skip limit page

--- controllers/products.js
+++ controllers/products.js
@@ -4,9 +4,9 @@ const product = require("../models/product")
 const getAllproductsStatic = async (req,res)=>{
 
   const products = await product.find({
-    // featured:true
-    name:"wooden table"
-  })
+    featured:true
+    // name:"wooden table"
+  }).sort('name').select('name price').limit(3).skip(1)
 
   return res.status(200).json({products,noProducts:products.length})
 
@@ -22,7 +22,7 @@ const getAllproducts = async (req,res)=>{
   // const products = await product.find(req.query)
 
 
-  const {featured,company,name,sort} = req.query
+  const {featured,company,name,sort,field} = req.query
 
   queryQbject={}
   if(featured){
@@ -40,7 +40,25 @@ const getAllproducts = async (req,res)=>{
   if(sort){
     const sortList =sort.split(',').join(' ');
     result = result.sort(sortList);
+  }else{
+    result = result.sort('createdAt');
   }
+
+
+  if(field){
+    const fieldList = field.split(',').join(' ');
+    result =result.select(fieldList)
+  }
+  
+  const page =Number(req.query.page) || 1;
+  const limit =Number(req.query.limit) || 10;
+
+  const skip= (page-1)*limit;
+
+  result=result.skip(skip).limit(limit)
+
+
+  
   const products = await result;
 
 
